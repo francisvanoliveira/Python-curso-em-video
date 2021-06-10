89f3d32f8c9e4dd9fa966af37e45ae5f2162f04e
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2021-06-10 14:11:13 -0400

    Exercício
    Atualização do exercício 21

--- Exercícios/ex021.py
+++ Exercícios/ex021.py
@@ -1,3 +1,10 @@
 #Faça um programa em pyton que abra e respoduza o áudio de arquivo em mp3.
 
 import pygame
+pygame.mixer.init()
+pygame.init()
+
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
